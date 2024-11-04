80e3fe91fb2a277a426e37af8d213f0c36ae9c0e
abyf <abyf2012@yahoo.fr>
2024-11-04 09:58:28 +0000

    Video13, Add Product to Cart completed

--- totemojapan/cart/cart.py
+++ totemojapan/cart/cart.py
@@ -11,3 +11,13 @@ class Cart():
 
         # Make sure cart is available on all pages of sites
         self.cart = cart
+
+    def add(self, product):
+        product_id = str(product.id)
+
+        if product_id in self.cart:
+            pass
+        else:
+            self.cart[product_id] = {'price': str(product.price)}
+        
+        self.session.modified = True
