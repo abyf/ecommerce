35d5e3807449b24ce1db011670a54e157c344dfd
abyf <abyf2012@yahoo.fr>
2024-11-09 13:09:18 +0000

    Video26, Search Products - Completed

--- totemojapan/store/views.py
+++ totemojapan/store/views.py
@@ -6,6 +6,21 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .forms import SignUpForm, UpdateUserForm, ChangePasswordForm, UserInfoForm
 from django import forms
+from django.db.models import Q
+
+def search(request):
+    # Determine if they filled out the form
+    if request.method == "POST":
+        searched = request.POST['searched']
+        # Let's query the Products DB Model
+        searched = Product.objects.filter(Q(name__icontains=searched) | Q(description__icontains=searched))
+        if not searched:
+            messages.success(request,("That Product Does Not Exist... Please Try Again"))
+            return render(request,"search.html",{})
+        else:
+            return render(request,"search.html",{'searched':searched})
+    else:
+        return render(request,"search.html",{})
 
 def update_info(request):
     if request.user.is_authenticated:
