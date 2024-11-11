845a7d2b6d90f34b8a27a41015ba38da3e5d3413
abyf <abyf2012@yahoo.fr>
2024-11-11 22:30:12 +0000

    Video7, Order Items Inlines, Completed

--- totemojapan/payment/views.py
+++ totemojapan/payment/views.py
@@ -47,6 +47,13 @@ def process_order(request):
                         create_order_item = OrderItem(order_id=order_id, product_id=product_id, user=user, quantity=value, price=price)
                         create_order_item.save()
 
+            # Delete our cart
+            for key in list(request.session.keys()):
+                if key == "session_key":
+                    # Delete the Key
+                    del request.session[key]
+
+
             messages.success(request, "Order Placed!")
             return redirect('home') 
         else:
@@ -70,6 +77,12 @@ def process_order(request):
                         # Create order item
                         create_order_item = OrderItem(order_id=order_id, product_id=product_id, quantity=value, price=price)
                         create_order_item.save()
+
+            # Delete our cart
+            for key in list(request.session.keys()):
+                if key == "session_key":
+                    # Delete the Key
+                    del request.session[key]
 
             messages.success(request, "Order Placed!")
             return redirect('home') 
