ed58532830f1c11cd2c1a2c388abb52eb83e2df7
abyf <abyf2012@yahoo.fr>
2024-11-11 22:04:33 +0000

    Video36, Attach Order Items, Completed

--- totemojapan/payment/views.py
+++ totemojapan/payment/views.py
@@ -4,6 +4,7 @@ from payment.forms import ShippingForm, PaymentForm
 from payment.models import ShippingAddress, Order, OrderItem
 from django.contrib.auth.models import User
 from django.contrib import messages
+from store.models import Product
 
 def process_order(request):
     if request.POST:
@@ -29,12 +30,47 @@ def process_order(request):
             # create Order
             create_order = Order(user=user, full_name=full_name, email=email, shipping_address=shipping_address, amount_paid=amount_paid)
             create_order.save()
+            # Get the order ID
+            order_id = create_order.pk
+            # Get Product Info
+            for product in cart_products():
+                product_id = product.id
+                if product.is_sale:
+                    price = product.sale_price
+                else:
+                    price = product.price
+
+                # Get Quantity
+                for key,value in quantities().items():
+                    if int(key) == product_id:
+                        # Create order item
+                        create_order_item = OrderItem(order_id=order_id, product_id=product_id, user=user, quantity=value, price=price)
+                        create_order_item.save()
+
             messages.success(request, "Order Placed!")
             return redirect('home') 
         else:
             # Not logged in
             create_order = Order(full_name=full_name, email=email, shipping_address=shipping_address, amount_paid=amount_paid)
             create_order.save()
+
+            # Get the order ID
+            order_id = create_order.pk
+            # Get Product Info
+            for product in cart_products():
+                product_id = product.id
+                if product.is_sale:
+                    price = product.sale_price
+                else:
+                    price = product.price
+
+                # Get Quantity
+                for key,value in quantities().items():
+                    if int(key) == product_id:
+                        # Create order item
+                        create_order_item = OrderItem(order_id=order_id, product_id=product_id, quantity=value, price=price)
+                        create_order_item.save()
+
             messages.success(request, "Order Placed!")
             return redirect('home') 
         
